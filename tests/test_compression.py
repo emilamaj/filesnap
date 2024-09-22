b24ce23deadd7a51cab995e8596e38340f59dae1
Emile Amajar <74051072+emilamaj@users.noreply.github.com>
2024-09-22 17:39:33 +0200

    Add compression tests

--- tests/test_compression.py
+++ tests/test_compression.py
@@ -3,7 +3,7 @@ from pyfilesnap.utils import compress_data, decompress_data
 
 class TestCompression(unittest.TestCase):
     def test_compression_decompression(self):
-        original_data = b'This is some test data for compression. ' * 10  # Make the test string longer
+        original_data = b'This is some test data for compression. ' * 10
         
         compressed = compress_data(original_data)
         decompressed = decompress_data(compressed)
